8e7dcb48c3560e1bd08aeab82618999ff8fa5ed8
leoho0722 <leo160918@gmail.com>
2024-09-08 15:52:14 +0800

    args 新增 cache_dir

--- downloader.py
+++ downloader.py
@@ -1,8 +1,10 @@
 from huggingface_hub import snapshot_download
+from huggingface_hub.constants import HF_HUB_CACHE
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-def model_download(model_name: str):
+
+def model_download(args):
     """
     從 HuggingFace 下載模型
 
@@ -11,17 +13,28 @@ def model_download(model_name: str):
     """
 
     snapshot_download(
-        repo_id=model_name
+        repo_id=args.model,
+        cache_dir=args.cache_dir,
     )
+
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--model", help="Model name, Ex: meta-llama/Meta-Llama-3-8B")
+    parser.add_argument(
+        "-m", "--model",
+        required=True,
+        help="Model name, Ex: meta-llama/Meta-Llama-3-8B"
+    )
+    parser.add_argument(
+        "--cache_dir",
+        default=HF_HUB_CACHE,
+        help="Model cache directory"
+    )
     args = parser.parse_args()
 
     if args.model is None:
         raise ValueError("model name is required")
-    
-    model_download(args.model)
+
+    model_download(args)
